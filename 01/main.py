5efaba908c42ea4c68116d3024620d831cdedd45
Friedrich Sonntag <friedrich@next-data-service.com>
2022-12-14 18:02:43 +0100

    01

---
+++ 01/main.py
@@ -0,0 +1,17 @@
+
+
+with open("input.txt") as input_file:
+    lines = input_file.readlines()
+
+highest_tripple = [0, 0, 0]
+current_max = 0
+for line in lines:
+    if line != '\n':
+        current_max += int(line.strip('\n'))
+    else:
+        minimum = min(highest_tripple)
+        if current_max > minimum:
+            highest_tripple[highest_tripple.index(minimum)] = current_max
+        current_max = 0
+
+print(sum(highest_tripple))
